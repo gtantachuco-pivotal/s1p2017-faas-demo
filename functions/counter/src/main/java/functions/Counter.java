6e4941432426af1de3369cc43e645c2a01a10884
markfisher <mfisher@pivotal.io>
2017-11-28 14:07:49 -0500

    temp: for testing pipes dispatcher

--- functions/counter/src/main/java/functions/Counter.java
+++ functions/counter/src/main/java/functions/Counter.java
@@ -1,5 +1,6 @@
 package functions;
 
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
@@ -9,7 +10,7 @@ import reactor.core.publisher.Flux;
 public class Counter implements Function<Flux<String>, Flux<Map<String,Integer>>> {
 
 	public Flux<Map<String, Integer>> apply(Flux<String> words) {
-		return words.window(10)
+		return words.window(Duration.ofSeconds(10))
 				.flatMap(f -> f.flatMap(word -> Flux.fromArray(word.split("\\W")))
 				.reduce(new HashMap<String, Integer>(), (map, word) -> {
 						map.merge(word, 1, Integer::sum);
